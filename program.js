ce4660601e88e4bc689a683304e383b5136e8b04
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-30 11:56:57 +0200

    Add js file

---
+++ program.js
@@ -0,0 +1,41 @@
+process.stdin.setEncoding('utf-8');
+
+function timeCalculator(seconds) {
+
+
+
+	var seconds= process.stdin.read();
+    if (seconds !== null) {
+        var instruction = seconds.toString().trim();
+    
+
+    var minutes = Math.floor(seconds / 60),
+        hours = Math.floor(seconds / 3600);
+
+	    switch (instruction) {
+
+	    	case seconds = NaN:
+	    		process.stdout.write("Wpisano złą wartość\n");
+			    process.exit();
+			    break;
+
+			case seconds < 0:
+	    		process.stdout.write("Wpisano ujemną wartość\n");
+			    process.exit();
+			    break;
+
+			case seconds > 0 & seconds <! 3600:
+	    		process.stdout.write(minutes);
+			    process.exit();
+			    break;
+
+			case seconds > 3600:
+	    		process.stdout.write(hours);
+			    process.exit();
+			    break;
+	    }
+
+	}
+}
+
+exports.print = timeCalculator;
