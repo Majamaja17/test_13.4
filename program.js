52e3468bb820b2b3e553dd8519265bd74b6c01ff
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-30 12:00:07 +0200

    Add js file

--- program.js
+++ program.js
@@ -2,40 +2,38 @@ process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
 
-	function timeCalculator(seconds) {
-		var seconds= process.stdin.read();
-	    if (seconds !== null) {
-	        var instruction = seconds.toString().trim();
-	    
+	var seconds= process.stdin.read();
+    if (seconds !== null) {
+        var instruction = seconds.toString().trim();
+    
 
-	    var minutes = Math.floor(seconds / 60),
-	        hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor(seconds / 60),
+        hours = Math.floor(seconds / 3600);
 
-		    switch (instruction) {
+	    switch (instruction) {
 
-		    	case seconds = NaN:
-		    		process.stdout.write("Wpisano złą wartość\n");
-				    process.exit();
-				    break;
+	    	case seconds = NaN:
+	    		process.stdout.write("Wpisano złą wartość\n");
+			    process.exit();
+			    break;
 
-				case seconds < 0:
-		    		process.stdout.write("Wpisano ujemną wartość\n");
-				    process.exit();
-				    break;
+			case seconds < 0:
+	    		process.stdout.write("Wpisano ujemną wartość\n");
+			    process.exit();
+			    break;
 
-				case seconds > 0 & seconds <! 3600:
-		    		process.stdout.write(minutes);
-				    process.exit();
-				    break;
+			case seconds > 0 & seconds <! 3600:
+	    		process.stdout.write(minutes);
+			    process.exit();
+			    break;
 
-				case seconds > 3600:
-		    		process.stdout.write(hours);
-				    process.exit();
-				    break;
-		    }
+			case seconds > 3600:
+	    		process.stdout.write(hours);
+			    process.exit();
+			    break;
+	    }
 
-		}
 	}
 });
 
-exports.print = timeCalculator;
+exports.print = readable;
